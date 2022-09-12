3162f039d7421040000e26d56a4a52becd1ffa4b
murata0531 <coless115@gmail.com>
2022-09-12 18:36:55 +0900

    特殊文字のエスケープ、空白の除去

--- src/library_action.php
+++ src/library_action.php
@@ -4,20 +4,20 @@ include 'classes/Library.php';
 $lib_obj = new Library;
 
 if(isset($_POST['save-book'])){
-    $book_name = $_POST['book-name'];
-    $book_genre = $_POST['book-genre'];
-    $book_author = $_POST['book-author'];
-    $date_added = $_POST['book-date-added'];
+    $book_name = trim(htmlspecialchars($_POST['book-name']));
+    $book_genre = trim(htmlspecialchars($_POST['book-genre']));
+    $book_author = trim(htmlspecialchars($_POST['book-author']));
+    $date_added = trim(htmlspecialchars($_POST['book-date-added']));
 
     $lib_obj->add_book($book_name,$book_genre,$book_author,$date_added);
 
     
 }elseif(isset($_POST['update-book'])){
-    $book_id = $_POST['book_id'];
-    $book_name = $_POST['book-name'];
-    $book_genre = $_POST['book-genre'];
-    $book_author = $_POST['book-author'];
-    $date_added = $_POST['book-date-added'];
+    $book_id = trim(htmlspecialchars($_POST['book_id']));
+    $book_name = trim(htmlspecialchars($_POST['book-name']));
+    $book_genre = trim(htmlspecialchars($_POST['book-genre']));
+    $book_author = trim(htmlspecialchars($_POST['book-author']));
+    $date_added = trim(htmlspecialchars($_POST['book-date-added']));
 
     $lib_obj->update_book($book_name,$book_genre,$book_author,$date_added,$book_id);
    
