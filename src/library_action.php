d75cbf11ea1c09d32cb1e4728c031cb842dd7e5d
murata0531 <coless115@gmail.com>
2022-09-12 18:53:10 +0900

    文字列をエスケープ

--- src/library_action.php
+++ src/library_action.php
@@ -4,20 +4,20 @@ include 'classes/Library.php';
 $lib_obj = new Library;
 
 if(isset($_POST['save-book'])){
-    $book_name = trim(htmlspecialchars($_POST['book-name']));
-    $book_genre = trim(htmlspecialchars($_POST['book-genre']));
-    $book_author = trim(htmlspecialchars($_POST['book-author']));
-    $date_added = trim(htmlspecialchars($_POST['book-date-added']));
+    $book_name = trim(htmlspecialchars($_POST['book-name'], ENT_QUOTES, 'UTF-8'));
+    $book_genre = trim(htmlspecialchars($_POST['book-genre'], ENT_QUOTES, 'UTF-8'));
+    $book_author = trim(htmlspecialchars($_POST['book-author'], ENT_QUOTES, 'UTF-8'));
+    $date_added = trim(htmlspecialchars($_POST['book-date-added'], ENT_QUOTES, 'UTF-8'));
 
     $lib_obj->add_book($book_name,$book_genre,$book_author,$date_added);
 
     
 }elseif(isset($_POST['update-book'])){
-    $book_id = trim(htmlspecialchars($_POST['book_id']));
-    $book_name = trim(htmlspecialchars($_POST['book-name']));
-    $book_genre = trim(htmlspecialchars($_POST['book-genre']));
-    $book_author = trim(htmlspecialchars($_POST['book-author']));
-    $date_added = trim(htmlspecialchars($_POST['book-date-added']));
+    $book_id = trim(htmlspecialchars($_POST['book_id'], ENT_QUOTES, 'UTF-8'));
+    $book_name = trim(htmlspecialchars($_POST['book-name'], ENT_QUOTES, 'UTF-8'));
+    $book_genre = trim(htmlspecialchars($_POST['book-genre'], ENT_QUOTES, 'UTF-8'));
+    $book_author = trim(htmlspecialchars($_POST['book-author'], ENT_QUOTES, 'UTF-8'));
+    $date_added = trim(htmlspecialchars($_POST['book-date-added'], ENT_QUOTES, 'UTF-8'));
 
     $lib_obj->update_book($book_name,$book_genre,$book_author,$date_added,$book_id);
    
