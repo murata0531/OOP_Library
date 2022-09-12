a3a570aa9895eb0d0a42d36d0ad4de36461ada92
murata0531 <coless115@gmail.com>
2022-09-12 17:10:08 +0900

    libraryデータベースを作りbooksテーブルを追加する

--- init/init.sql
+++ init/init.sql
@@ -1,2 +1,12 @@
-CREATE DATABASE IF NOT EXISTS `test_db2`;
-GRANT ALL ON test_db2.* TO 'test_user'@'%';
+CREATE DATABASE IF NOT EXISTS `library`;
+GRANT ALL ON library.* TO 'test_user'@'%';
+
+use library;
+
+CREATE TABLE `books` (
+  `book_id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `book_name` varchar(100) NOT NULL,
+  `book_genre` varchar(100) NOT NULL,
+  `book_author` varchar(100) NOT NULL,
+  `date_added` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
